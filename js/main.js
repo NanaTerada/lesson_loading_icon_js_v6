7a4c62044b66949dda23bcd22e005f7c165285e5
n2 <7worki.nprogress@gmail.com>
2020-05-14 19:56:21 +0900

    #07 描画関連の処理を切り出そう

--- js/main.js
+++ js/main.js
@@ -1,49 +1,58 @@
 // 'use strict'
 
-// 即時関数？？！'use strict'あるとエラーになる・・
-// returnを使うために関数にする。returnがブロックではつかえないので
+
 
     (() => {
-      class Icon {
-          constructor(canvas) {
-            this.ctx = canvas.getContext('2d');
+        class IconDrawer {
+            constructor(canvas) {
+                this.ctx = canvas.getContext('2d');
             this.width = canvas.width;
             this.height = canvas.height;
             this.r = 60;
 
+            }
+
+            draw(angle) {
+
+                this.ctx.fillStyle = 'rgba(255,255,255,0.3)';
+                this.ctx.fillRect(0,0,this.width,this.height);
+                this.ctx.save();
+    
+    
+                
+                    this.ctx.translate(this.width / 2,this.height / 2);
+                    this.ctx.rotate(Math.PI / 180 * angle);
+    
+                
+    
+                this.ctx.beginPath();
+                
+                this.ctx.moveTo(0 , -this.r -5);
+                this.ctx.lineTo(0 , -this.r +5);
+              
+                this.ctx.strokeStyle ='orenge';　
+                this.ctx.lineWidth = 6;
+                this.ctx.stroke();
+    
+                
+    
+              this.ctx.restore();
+              }
+
+        }
+
+
+      class Icon {
+          constructor(drawer) {
+              this.drawer = drawer;
             this.angle = 0;
           }
 
           draw() {
-
-            this.ctx.fillStyle = 'rgba(255,255,255,0.3)';
-            this.ctx.fillRect(0,0,this.width,this.height);
-            this.ctx.save();
-
-
-            // 座標空間を回転させる
-                this.ctx.translate(this.width / 2,this.height / 2);
-                this.ctx.rotate(Math.PI / 180 * this.angle);
-
-            // this.ctx.beginPath();
-            // //this.ctx.arc(this.width / 2,this.height / 2, this.r ,0 ,2 * Math.PI);
-            // this.ctx.arc(0 ,0 , this.r ,0 ,2 * Math.PI);
-            // this.ctx.stroke();
-
-            this.ctx.beginPath();
-            // this.ctx.moveTo(this.width / 2,this.height / 2 -this.r -5);
-            // this.ctx.lineTo(this.width / 2,this.height / 2 -this.r +5);
-            this.ctx.moveTo(0 , -this.r -5);
-            this.ctx.lineTo(0 , -this.r +5);
-           // this.ctx.strokeStyle = 'orenge';
-            this.ctx.strokeStyle ='orenge';　//色が変わらない！！；；
-            this.ctx.lineWidth = 6;
-            this.ctx.stroke();
-
-            
-
-          this.ctx.restore();
+              this.drawer.draw(this.angle);
           }
+
+          
 
           update() {
               this.angle += 12;
@@ -61,11 +70,11 @@
       }
 
         const canvas = document.querySelector('canvas');
-    if (typeof canvas.getContext　== 'undefined') { //canvas がサポートされていなかった場合
+    if (typeof canvas.getContext　== 'undefined') { 
         return;
     }
 
-    const icon = new Icon(canvas);
+    const icon = new Icon(new IconDrawer(canvas));
     icon.run();
 
     })();
