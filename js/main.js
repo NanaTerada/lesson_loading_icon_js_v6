86f8cba1ddd50288c82efce1bc0f8ce7b7672a77
n2 <7worki.nprogress@gmail.com>
2020-05-14 18:07:46 +0900

    #03 円を描画してみよう

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-'use strict'
+// 'use strict'
 
-// 即時関数？？！
+// 即時関数？？！'use strict'あるとエラーになる・・
 // returnを使うために関数にする。returnがブロックではつかえないので
 
     (() => {
       class Icon {
           constructor(canvas) {
+            this.ctx = canvas.getContext('2d');
+            this.width = canvas.width;
+            this.height = canvas.height;
+            this.r = 60;
+          }
 
+          draw() {
+            this.ctx.beginPath();
+            this.ctx.arc(this.width / 2,this.height / 2, this.r ,0 ,2 * Math.PI);
+            this.ctx.stroke();
           }
 
           run() {
-              
+            this.draw();
           }
       }
 
@@ -19,7 +28,8 @@
         return;
     }
 
-    const icon = new icon(canvas);
+    const icon = new Icon(canvas);
     icon.run();
+
     })();
     
